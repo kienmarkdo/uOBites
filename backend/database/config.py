c64ad49325ae625296a6538ab65a28200b23fe3a
Kien Do <67518620+kienmarkdo@users.noreply.github.com>
2023-06-25 19:43:56 -0400

    Delete config.py

--- backend/database/config.py
+++
@@ -1,7 +0,0 @@
-# This config file is meant to store your postgres password for the db.py file
-
-# NOTE: This file should be ignored by the .gitignore file, so you can safely commit and push this file.
-# However, take great precaution as this is a sensitive file that contains your Postgres password
-
-# Double-check that this file is ignored by .gitignore before committing/pushing your password
-postgres_password = "ENTER_POSTGRES_PASSWORD_HERE"

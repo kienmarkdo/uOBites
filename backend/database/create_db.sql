a816c75b832d7ad649083bf19e24f33f0465ed7b
Kien Do <kienmarkdo@gmail.com>
2023-07-03 21:58:08 -0400

    DB redesign

--- backend/database/create_db.sql
+++ backend/database/create_db.sql
@@ -15,85 +15,25 @@ CREATE TABLE IF NOT EXISTS User_account (
     password VARCHAR(255) NOT NULL,
     firstName VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL,
-);
-
--- Admin Account
-CREATE TABLE IF NOT EXISTS Admin_account (
-    admin_id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    CONSTRAINT FK_user_id FOREIGN KEY(user_id) REFERENCES User_account(user_id) ON UPDATE CASCADE ON DELETE CASCADE
-);
-
--- Customer Account
-CREATE TABLE IF NOT EXISTS Customer_account (
-    customer_id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
     flex_card VARCHAR(255),
-    CONSTRAINT FK_user_id FOREIGN KEY(user_id) REFERENCES User_account(user_id) ON UPDATE CASCADE ON DELETE CASCADE
-);
-
--- Food Outlet Account / Vendor account
--- allows employees of a food outlet to login to their own outlet page
-CREATE TABLE IF NOT EXISTS Vendor_account (
-    vendor_id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    outlet_id INTEGER,
-    accepts_flex BOOLEAN,
-    CONSTRAINT FK_user_id FOREIGN KEY(user_id) REFERENCES User_account(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
-    -- each vendor acc has one food outlet
-    CONSTRAINT FK_outlet_id FOREIGN KEY(outlet_id) REFERENCES Food_outlet(outlet_id)
-);
-
--- Food Outlet
-CREATE TABLE IF NOT EXISTS Food_outlet (
-    outlet_id SERIAL PRIMARY KEY,
-    outlet_name VARCHAR(255),
-    outlet_location VARCHAR(255),
-    -- sum of all ratings
-    total_rating_points INTEGER,
-    -- the number of users who have rated this food outlet
-    total_num_of_ratings INTEGER,
-    -- Example of ratings:
-    -- total_rating_points = 4 + 5 + 3 + 2 (existing ratings)
-    -- total_num_of_ratings = 4 (people who rated)
-    -- to calculate food outlet's ratings: rating = total_rating_points / total_num_of_ratings
-    -- add a new rating from user Bob, who gives a rating of 4
-    -- total_rating_points = (4 + 5 + 3 + 2) + 4
-    -- total_num_of_ratings = 5 + 1
-    CONSTRAINT total_rating_points >= 0,
-    CONSTRAINT total_num_of_ratings >= 0
 );
 
 -- Food Orders
+-- only display total price of the order
+-- does not display what items were ordered
+-- to display order history of user A, traverse through this table and
+-- find all orders that belong to user A via foreign key
+-- user must wait on the Order Success popout screen. If they close that screen,
+-- this order becomes "Cancelled" to signify that the user left the page.
+-- the user be refunded...
 CREATE TABLE IF NOT EXISTS Food_orders (
     order_id SERIAL PRIMARY KEY,
     order_status VARCHAR(255),
     total_price NUMERIC(10, 2),
-    --fk
-    vendor_id INTEGER NOT NULL,
-    --fk
+    -- fk: id of the customer to which this food order belongs
     cust_id INTEGER NOT NULL,
-    --fk
-    CONSTRAINT FK_vendor_id FOREIGN KEY(vendor_id) REFERENCES Vendor_account(vendor_id) ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT FK_cust_id FOREIGN KEY(cust_id) REFERENCES Customer_account(cust_id) ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT Status_check CHECK(
         order_status IN ('In Progress', 'Ready', 'Completed', 'Cancelled')
     ) -- status must be In Progress, Ready, Completed, or Cancelled
-);
-
--- Menus of all outlets
-CREATE TABLE IF NOT EXISTS Menu_item (
-    item_id SERIAL PRIMARY KEY,
-    item_name VARCHAR(255),
-    price NUMERIC(10, 2),
-    --fk
-    outlet_id INTEGER NOT NULL,
-    --fk
-    cust_id INTEGER NOT NULL,
-    --fk for customer for itemsOrdered[], tho i find it might be easier to attach this to Food_orders instead
-    availability BOOLEAN,
-    CONSTRAINT FK_outlet_id FOREIGN KEY(outlet_id) REFERENCES Food_outlet(outlet_id) --cascade or not?
-    ON UPDATE CASCADE,
-    CONSTRAINT FK_cust_id FOREIGN KEY(cust_id) REFERENCES Customer_account(cust_id) --do not delete cascade because we don't want menu item to be deleted if a customer or order is deleted
-    ON UPDATE CASCADE
 );

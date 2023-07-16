d965147754287bd1acb5e62a2d210d2a5b6ae568
Kanjanokphat Kitisuwanakul <77587871+AmyKKiti@users.noreply.github.com>
2023-07-15 20:15:45 -0400

    Updated food outlets logo

--- frontend/src/screens/LandingPage.tsx
+++ frontend/src/screens/LandingPage.tsx
@@ -5,15 +5,15 @@ import { useNavigate } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import StoreIcon from './components/StoreIcon';
 import BentoSushiLogo from "../images/bento_sushi_logo.png";
-import ThaiExpressLogo from "../images/thai_express_logo.png";
 import PremiereMoissonLogo from "../images/premiere_moisson_logo.png";
 import TimHortonLogo from "../images/tim_hortons_logo.png";
 import LeBacAFrites from "../images/le_bac_a_frites_logo.png";
 import StarbucksLogo from "../images/starbucks_logo.png";
-import GoCafeLogo from "../images/go_cafe_logo.png";
 import FlourKitchenLogo from "../images/flour_kitchen_logo.png";
 import SecondCupLogo from "../images/second_cup_logo.png";
 import ParamountLogo from "../images/paramount_logo.png";
+import JuiceBarLogo from "../images/juice_bar_logo.png";
+import Pizza800Logo from "../images/pizza_800_logo.png";
 import appLogo from "../images/app-logo.png"
 
 // idk if there's a better way to do it, cause the import statements gets long as we continue adding all the stores
@@ -24,16 +24,17 @@ const LandingPage = () => {
   const navigate = useNavigate();
 
   const storesInfo = [
-    { storeName: "Bento Sushi", imageSrc: BentoSushiLogo },
-    { storeName: "Premiere Moisson", imageSrc: PremiereMoissonLogo },
-    { storeName: "Tim Hortons", imageSrc: TimHortonLogo },
-    { storeName: "Thai Express", imageSrc: ThaiExpressLogo },
-    { storeName: "Le Bac À Frites", imageSrc: LeBacAFrites }, 
-    { storeName: "Starbucks", imageSrc: StarbucksLogo },
-    { storeName: "Go Café", imageSrc: GoCafeLogo },
+    { storeName: "Bento Sushi (UCU)", imageSrc: BentoSushiLogo },
+    { storeName: "Premiere Moisson (FSS)", imageSrc: PremiereMoissonLogo },
+    { storeName: "Tim Hortons (CRX)", imageSrc: TimHortonLogo },
+    { storeName: "Tim Hortons (SITE)", imageSrc: TimHortonLogo },
+    { storeName: "Le Bac À Frites (Parking lot K)", imageSrc: LeBacAFrites }, 
+    { storeName: "Starbucks (DMS)", imageSrc: StarbucksLogo },
     { storeName: "Flour Kitchen", imageSrc: FlourKitchenLogo },
-    { storeName: "Second Cup", imageSrc: SecondCupLogo },
-    { storeName: "Paramount Lebanese Kitchen", imageSrc: ParamountLogo }
+    { storeName: "Second Cup (HSY)", imageSrc: SecondCupLogo },
+    { storeName: "Paramount (CRX)", imageSrc: ParamountLogo },
+    { storeName: "305 Juice bar (UCU)", imageSrc: JuiceBarLogo },
+    { storeName: "Pizza 800 (STE)", imageSrc: Pizza800Logo }
   ]
   
   return (

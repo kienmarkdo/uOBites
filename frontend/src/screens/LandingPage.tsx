a63d4805e024d0c0c2e8cebf01546d14125de5d1
Celine Wan <mwanm074@uottawa.ca>
2023-07-16 18:15:21 -0400

    Removed Second Cup and unused logos

--- frontend/src/screens/LandingPage.tsx
+++ frontend/src/screens/LandingPage.tsx
@@ -9,7 +9,6 @@ import TimHortonLogo from "../images/tim_hortons_logo.png";
 import LeBacAFrites from "../images/le_bac_a_frites_logo.png";
 import StarbucksLogo from "../images/starbucks_logo.png";
 import FlourKitchenLogo from "../images/flour_kitchen_logo.png";
-import SecondCupLogo from "../images/second_cup_logo.png";
 import ParamountLogo from "../images/paramount_logo.png";
 import JuiceBarLogo from "../images/juice_bar_logo.png";
 import Pizza800Logo from "../images/pizza_800_logo.png";
@@ -43,7 +42,6 @@ const LandingPage = () => {
     { storeName: "Le Bac À Frites (Parking lot K)", imageSrc: LeBacAFrites }, 
     { storeName: "Starbucks (DMS)", imageSrc: StarbucksLogo },
     { storeName: "Flour Kitchen", imageSrc: FlourKitchenLogo },
-    { storeName: "Second Cup (HSY)", imageSrc: SecondCupLogo },
     { storeName: "Paramount (CRX)", imageSrc: ParamountLogo },
     { storeName: "305 Juice bar (UCU)", imageSrc: JuiceBarLogo },
     { storeName: "Pizza 800 (STE)", imageSrc: Pizza800Logo }

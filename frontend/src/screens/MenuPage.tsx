2283ae348b47a4d61eeb8d3f1261a45a5f878334
Celine Wan <mwanm074@uottawa.ca>
2023-07-17 16:54:37 -0400

    Displayed the food items on the menu page

--- frontend/src/screens/MenuPage.tsx
+++ frontend/src/screens/MenuPage.tsx
@@ -5,9 +5,22 @@ import { Link, useLocation } from 'react-router-dom';
 import { Modal } from 'react-bootstrap';
 import Navbar from './components/Navbar';
 import OutletInfoJson from '../data/foodOutletsInfo.json';
-import OutletMenuJson from '../data/foodOutletsMenu.json';
+
+interface MenuItem {
+  id: number;
+  menu: {
+    [category: string]: {
+      [foodItem: string]: {
+        price: number;
+        image: string;
+      };
+    };
+  };
+}
 
 export default function MenuPage() {
+
+  const OutletMenuJson: MenuItem[] = require('../data/foodOutletsMenu.json');
 
   const location = useLocation();
   const info = location.state || {};
@@ -27,52 +40,56 @@ export default function MenuPage() {
           <Navbar />
           <h1 className='menu-title'>{info.outletName}</h1>
           <section className='menu-container'>
+            <div className='info-container'>
             <h2>Information</h2>
             <br />
-            {OutletInfoJson.map((outlet) => {
+              {OutletInfoJson.map((outlet) => {
                 if (outlet.id === info.outletId) {
                   return (
                     <>
-                    <p><strong>Location: </strong>{outlet.location}</p>
-                    <p><strong>Hours: </strong></p>
-                    <ul>
-                      {Object.entries(outlet.hoursOfOperation).map(([day, hours]) => (
-                        <li key={day}>
-                          <strong>{day}</strong> : {hours}
-                        </li>
-                      ))}
-                    </ul>
-                    <p><strong>Exception: </strong>{outlet['exception:']}</p>
-
-                    </>
-                    
+                      <p><strong>Location: </strong>{outlet.location}</p>
+                      <p><strong>Hours: </strong></p>
+                      <ul>
+                        {Object.entries(outlet.hoursOfOperation).map(([day, hours]) => (
+                          <li key={day}>
+                            <strong>{day}</strong> : {hours}
+                          </li>
+                        ))}
+                      </ul>
+                      <p><strong>Exception: </strong>{outlet['exception:']}</p>
+                    </>  
                   );
                 } else {
                   return <></>;
                 }
-            })}
+              })}
+              <br />
+              <hr />
+            </div>
+            <h2 className='text-center text-decoration-underline'>Menu</h2>
             <br />
-            <hr />
-            <br />
-            <h2>Menu</h2>
-            {/* {OutletMenuJson.map((outlet) => {
-                if (outlet.foodOutletId === info.outletId) {
-                  for (const category in outlet.menu) {
-                    console.log(category);
-                    console.log("--------------------");
-                  
-                    // Loop through the items within each category
-                    for (const item in outlet.menu[category]) {
-                      console.log("Item:", item);
-                      console.log("Price:", outlet.menu[category][item].price);
-                      console.log("Image:", outlet.menu[category][item].image);
-                      console.log("--------------------");
-                    }
-                  }
+              {OutletMenuJson.map((outlet) => {
+                if (outlet.id === info.outletId) {
+                  return Object.entries(outlet.menu).map(([category, items]) => (
+                    <div key={category} className='mx-1 px-5'>
+                      <h4 className='px-4'>{category}</h4>
+                      <div className='menu-list'>
+                        {Object.entries(items).map(([foodItem, item]) => (
+                          <div className='menu-item '>
+                            <h6>${item.price.toFixed(2)}</h6>
+                            <img src={item.image} alt={foodItem} width={150} />
+                            <div className='mt-1' style={{ width: 150 }}>
+                              <h6>{foodItem}</h6>
+                            </div>
+                          </div>  
+                        ))}
+                      </div>
+                    </div>
+                  ));
                 } else {
                   return <></>;
                 }
-            })} */}
+              })}
           </section>
         </>
       ) : (
